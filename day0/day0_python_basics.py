9d3084047802bbd4e82ce1fa4b0d9c0bfc7a96e7
14sharmaishika <14sharmaishika@gmail.com>
2025-08-17 00:31:42 +0530

    Create day0_python_basics.py

---
+++ day0/day0_python_basics.py
@@ -0,0 +1,20 @@
+1. Add 2 numbers (user input).
+print("enter first number ")
+i=int(input())
+print("enter second number ")
+j=int(input())
+def sum(a,b):
+    print(a+b)
+
+print(sum(i,j))
+
+
+Calculate simple interest.
+p=10
+r=2
+t=20
+def SI(k,l,f):
+    q=k*l*f
+    print(q)
+
+print(SI(p,r,t))
